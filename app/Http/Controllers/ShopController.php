15df494b48cda1b8a8c8b143908fefbd641562d9
AmirMahdi-for <97610864+AmirMahdi-for@users.noreply.github.com>
2023-01-10 11:49:37 +0330

    13-write data in data

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Shop;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
@@ -27,6 +28,7 @@ class ShopController extends Controller
 
     public function store(Request $request)
     {
+        // ----------validate request of DATA ----------------
         $data = $request->validate([
             'title' => 'required|string|unique:shops,title|between:3,100',
             'first_name' => 'required|string',
@@ -35,7 +37,26 @@ class ShopController extends Controller
             'email' => 'required|email|unique:users,email',
             'username' => 'required|unique:users,name',
             'address' => 'nullable',
+        ]);
 
+        // ------------- create user in db -------------------
+        $randomPass = rand(1000, 9999);
+        $user = User::create([
+            'name' => $request->username,
+            'email' => $request->email,
+            'role' => 'shop',
+            'email_verified_at' => now(),
+            'password' => bcrypt($randomPass),
+        ]);
+
+        // ------------- create shop in db --------------------
+        Shop::create([
+            'user_id' => $user->id,
+            'title' => $request->title,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'phone' => $request->phone,
+            'address' => $request->address,
         ]);
     }
 
